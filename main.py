12fad8731eaa494c0ca32d672ea0f6217be626b7
Enchan <enchant0130@gmail.com>
2020-10-16 06:43:16 +0900

    [Modify] comment

--- main.py
+++ main.py
@@ -4,23 +4,23 @@
 import math
 
 def main():
-    # 初期条件やら最大パルスやら
+    # 初期条件
     f0 = 10
     fn = 500
     Nmax = 400
     Tclk = 100E-6 # 0.1ms ITU0の出力
 
-    # 加速度を求める
+    # 加速度を計算
     a = (f0 *f0 + fn * fn) / (2 * Nmax)
     print("accel: {0:.3f}".format(a))
 
-    # リスト内包表記でワンライン生成
+    # パルス周波数を計算
     pulseFreqs = [calcPulseFreq(f0, a, n) for n in range(Nmax)]
 
     # GRA1の値に変換
     graValues = [(1 / freq) / Tclk - 1 for freq in pulseFreqs]
 
-    # ファイルに吐き出す
+    # ファイルに出力
     with open("GRA1Table.csv", "w") as f:
         f.write(",\n".join(["{0:.0f}".format(f) for f in graValues]))
     
