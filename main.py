57327cc9eb24a23e67d9d224ff5c9071abbf913d
Enchan <enchant0130@gmail.com>
2020-10-15 17:15:43 +0900

    [Update] calucurate pulse freqs and export to file

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 #
 # パルス周波数テーブル作る
 #
+import math
 
 def main():
-    pass
+    # 初期条件やら最大パルスやら
+    f0 = 0
+    a = 2
+    Nmax = 400
 
-    
+    # リスト内包表記でワンライン生成
+    freqs = [calcPulseFreq(f0, a, n) for n in range(Nmax)]
 
+    # ファイルに吐き出す
+    with open("ferqTable.csv", "w") as f:
+        f.write(",\n".join(["{0:.3f}".format(f) for f in freqs]))
+
+# パルス周波数を計算する
+def calcPulseFreq(f0, a, n):
+    fn = math.sqrt(f0 * f0 + 2 * a * n)
+    return fn
 
 if __name__ == "__main__":
     main()
