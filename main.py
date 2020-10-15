f9aee31fd21ab0a8250764e2d6e9e7339b43d028
Enchan <enchant0130@gmail.com>
2020-10-15 19:53:31 +0900

    [Update] Interactive accell calcuration

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import math
 
 def main():
     # 初期条件やら最大パルスやら
-    f0 = 0
-    a = 2
+    f0 = 10
+    fn = 500
     Nmax = 400
+    Tclk = 100E-6 # 0.1ms ITU0の出力
+
+    # 加速度を求める
+    a = (f0 *f0 + fn * fn) / (2 * Nmax)
+    print("accel: {0:.3f}".format(a))
 
     # リスト内包表記でワンライン生成
-    freqs = [calcPulseFreq(f0, a, n) for n in range(Nmax)]
+    pulseFreqs = [calcPulseFreq(f0, a, n) for n in range(Nmax)]
+
+    # GRA1の値に変換
+    graValues = [(1 / freq) / Tclk - 1 for freq in pulseFreqs]
 
     # ファイルに吐き出す
-    with open("ferqTable.csv", "w") as f:
-        f.write(",\n".join(["{0:.3f}".format(f) for f in freqs]))
+    with open("GRA1Table.csv", "w") as f:
+        f.write(",\n".join(["{0:.0f}".format(f) for f in graValues]))
+    
+    with open("freqTable.csv", "w") as f:
+        f.write(",\n".join(["{0:.3f}".format(f) for f in pulseFreqs]))
 
 # パルス周波数を計算する
 def calcPulseFreq(f0, a, n):
