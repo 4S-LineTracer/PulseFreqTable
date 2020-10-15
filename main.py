a62ab6b872c5732aaa7e8324cf9d209e9738823b
Enchan <enchant0130@gmail.com>
2020-10-15 17:08:50 +0900

    [Add] main / README

---
+++ main.py
@@ -0,0 +1,18 @@
+#
+# パルス周波数テーブル作る
+#
+
+def main():
+    pass
+
+    
+
+
+if __name__ == "__main__":
+    main()
+    try:
+        print("Type Ctrl+C to exit.")
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
